a334371488031b257b241e225502a4328061d12a
Lokisk1155 <shawn12345d@gmail.com>
2022-09-19 09:37:15 -0400

    map/player works

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
-
-const ctx = canvas.getContext('2d')
 
 ctx.fillStyle = 'white'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
-image.src = 'PelletTown.png'
+image.src = 'map.png'
 
+const playerImage = new Image() 
+playerImage.src = 'playerDown.png'
 
-image.onLoad = () => { 
-    console.log(ctx)
+image.onload = () => { 
     ctx.drawImage(image, 0, 0)
+    ctx.drawImage(
+        playerImage, 
+        0, 
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - (playerImage.width / 4) / 2, 
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+        )
 }
 
 
